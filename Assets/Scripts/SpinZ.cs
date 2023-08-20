74cb3273f80bf25b528543989e26342704b1a897
Abhinav Agarwalla <120122716+ABHINAV231204@users.noreply.github.com>
2023-08-20 18:43:57 +0530

    commit

--- Assets/Scripts/SpinZ.cs
+++ Assets/Scripts/SpinZ.cs
@@ -7,6 +7,11 @@ public class SpinZ : MonoBehaviour
     public float RotateSpeed;
     void Update()
     {
+        Debug.Log("in");
         transform.Rotate(0f, 0f, RotateSpeed, Space.Self);
     }
+
+   
+
+
 }
